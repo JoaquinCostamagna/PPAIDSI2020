5622c62afa9468e21ddd3d38c00b8fbefa50ca65
JoaquinCostamagna <jncostamagna@gmail.com>
2020-10-19 17:05:42 -0300

    Implementacion pantalla principal

---
+++ RestaurantePPAI/RestaurantePPAI/pantallaAdmPedidos.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RestaurantePPAI
+{
+    partial class pantallaAdmPedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDetallePedido = new System.Windows.Forms.DataGridView();
+            this.nombreProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreMenu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numeroMesa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpEnPreparacion = new System.Windows.Forms.GroupBox();
+            this.dgvSeleccionados = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpSeleccionado = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetallePedido)).BeginInit();
+            this.grpEnPreparacion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSeleccionados)).BeginInit();
+            this.grpSeleccionado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDetallePedido
+            // 
+            this.dgvDetallePedido.AllowUserToAddRows = false;
+            this.dgvDetallePedido.AllowUserToDeleteRows = false;
+            this.dgvDetallePedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetallePedido.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombreProducto,
+            this.nombreMenu,
+            this.cantidad,
+            this.numeroMesa});
+            this.dgvDetallePedido.Location = new System.Drawing.Point(15, 19);
+            this.dgvDetallePedido.MultiSelect = false;
+            this.dgvDetallePedido.Name = "dgvDetallePedido";
+            this.dgvDetallePedido.ReadOnly = true;
+            this.dgvDetallePedido.Size = new System.Drawing.Size(1034, 268);
+            this.dgvDetallePedido.TabIndex = 1;
+            this.dgvDetallePedido.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetallePedido_CellContentClick);
+            // 
+            // nombreProducto
+            // 
+            this.nombreProducto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nombreProducto.HeaderText = "Nombre del Prodcuto";
+            this.nombreProducto.Name = "nombreProducto";
+            this.nombreProducto.ReadOnly = true;
+            // 
+            // nombreMenu
+            // 
+            this.nombreMenu.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nombreMenu.HeaderText = "Nombre del Menu";
+            this.nombreMenu.Name = "nombreMenu";
+            this.nombreMenu.ReadOnly = true;
+            // 
+            // cantidad
+            // 
+            this.cantidad.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.Name = "cantidad";
+            this.cantidad.ReadOnly = true;
+            this.cantidad.Width = 74;
+            // 
+            // numeroMesa
+            // 
+            this.numeroMesa.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.numeroMesa.HeaderText = "Numero/s de Mesa/s";
+            this.numeroMesa.Name = "numeroMesa";
+            this.numeroMesa.ReadOnly = true;
+            this.numeroMesa.Width = 89;
+            // 
+            // grpEnPreparacion
+            // 
+            this.grpEnPreparacion.Controls.Add(this.dgvDetallePedido);
+            this.grpEnPreparacion.Location = new System.Drawing.Point(39, 11);
+            this.grpEnPreparacion.Name = "grpEnPreparacion";
+            this.grpEnPreparacion.Size = new System.Drawing.Size(1055, 294);
+            this.grpEnPreparacion.TabIndex = 2;
+            this.grpEnPreparacion.TabStop = false;
+            this.grpEnPreparacion.Text = "Detalles en Preparacion";
+            // 
+            // dgvSeleccionados
+            // 
+            this.dgvSeleccionados.AllowUserToAddRows = false;
+            this.dgvSeleccionados.AllowUserToDeleteRows = false;
+            this.dgvSeleccionados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSeleccionados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dgvSeleccionados.Location = new System.Drawing.Point(16, 15);
+            this.dgvSeleccionados.MultiSelect = false;
+            this.dgvSeleccionados.Name = "dgvSeleccionados";
+            this.dgvSeleccionados.ReadOnly = true;
+            this.dgvSeleccionados.Size = new System.Drawing.Size(1033, 267);
+            this.dgvSeleccionados.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Nombre del Prodcuto";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nombre del Menu";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dataGridViewTextBoxColumn3.HeaderText = "Cantidad";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 74;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dataGridViewTextBoxColumn4.HeaderText = "Numero/s de Mesa/s";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 89;
+            // 
+            // grpSeleccionado
+            // 
+            this.grpSeleccionado.Controls.Add(this.dgvSeleccionados);
+            this.grpSeleccionado.Location = new System.Drawing.Point(39, 372);
+            this.grpSeleccionado.Name = "grpSeleccionado";
+            this.grpSeleccionado.Size = new System.Drawing.Size(1055, 288);
+            this.grpSeleccionado.TabIndex = 3;
+            this.grpSeleccionado.TabStop = false;
+            this.grpSeleccionado.Text = "Detalles seleccionados Listos Para Servir";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(998, 311);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(90, 33);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Location = new System.Drawing.Point(998, 677);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(90, 33);
+            this.btnQuitar.TabIndex = 5;
+            this.btnQuitar.Text = "Quitar";
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(530, 732);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(90, 33);
+            this.btnConfirmar.TabIndex = 6;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // pantallaAdmPedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 804);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnQuitar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.grpSeleccionado);
+            this.Controls.Add(this.grpEnPreparacion);
+            this.Name = "pantallaAdmPedidos";
+            this.Text = "Pedido";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetallePedido)).EndInit();
+            this.grpEnPreparacion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSeleccionados)).EndInit();
+            this.grpSeleccionado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDetallePedido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreMenu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numeroMesa;
+        private System.Windows.Forms.GroupBox grpEnPreparacion;
+        private System.Windows.Forms.DataGridView dgvSeleccionados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.GroupBox grpSeleccionado;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
---
+++ RestaurantePPAI/RestaurantePPAI/pantallaAdmPedidos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantePPAI
+{
+    public partial class pantallaAdmPedidos : Form
+    {
+        public pantallaAdmPedidos()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvDetallePedido_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
